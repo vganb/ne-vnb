00fe79a1cf744b18558dd560b85a5c87ddaaa018
Victor <victornorlanderble@gmail.com>
2024-10-15 10:12:13 +0200

    first commit setup project

---
+++ app/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import NeLogo from "./NeLogo";
+
+const Header = () => {
+  return (
+    <div className="flex gap-4 items-center border-4 border-red-900 h-20 rounded-md px-4 bg-orange-100">
+      <NeLogo />
+      <div className="text-xl text-black font-semibold w-32">
+        Nordic Experiences
+      </div>
+      <div>PROFILE</div>
+    </div>
+  );
+};
+
+export default Header;
