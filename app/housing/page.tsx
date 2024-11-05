afd1b382bbd90c8113889381ee97d1eb22e301f7
Victor <victornorlanderble@gmail.com>
2024-11-05 11:01:21 +0100

    Remove Suspense import from page.tsx

--- app/housing/page.tsx
+++ app/housing/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState, Suspense } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import HousingCard from "../components/HousingCard";
 import NavigationBottom from "../components/NavigationBottom";
